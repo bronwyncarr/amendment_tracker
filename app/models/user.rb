f4f074281273ff80451abebf0d98f8c14e87d71a
bronwyncarr <bronwynacarr@gmail.com>
2020-11-26 16:40:33 +1100

    devise sign in buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable and :omniauthable
+  # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable, :trackable
+         :recoverable, :rememberable, :validatable, :timeoutable
   
   has_many :records
 end
