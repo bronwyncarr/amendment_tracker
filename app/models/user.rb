5aa52b35d4d4603d493bf336dfa5ec3d76b175dc
bronwyncarr <bronwynacarr@gmail.com>
2020-11-27 11:15:56 +1100

    removes registable function from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :trackable, :registerable and :omniauthable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable, :timeoutable
   
   has_many :records
